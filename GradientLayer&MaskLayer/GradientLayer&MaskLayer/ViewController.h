ff56cb7ec6887141ecd9d4ee2f0cf6a96c40d1cf
LT_Skyrim <latorex_12@126.com>
2016-10-10 18:30:57 +0800

    create

---
+++ GradientLayer&MaskLayer/GradientLayer&MaskLayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GradientLayer&MaskLayer
+//
+//  Created by 梁天 on 16/10/10.
+//  Copyright © 2016年 梁天. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
