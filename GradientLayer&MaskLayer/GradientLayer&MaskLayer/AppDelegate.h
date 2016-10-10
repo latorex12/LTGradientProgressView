0c59c044950a1ddde8d27fa38df3696bb0628e01
LT_Skyrim <latorex_12@126.com>
2016-10-10 18:24:16 +0800

    Upload

---
+++ GradientLayer&MaskLayer/GradientLayer&MaskLayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GradientLayer&MaskLayer
+//
+//  Created by 梁天 on 16/10/9.
+//  Copyright © 2016年 梁天. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GradientLayer&MaskLayer/GradientLayer&MaskLayer/LTGradientProgressView.h
@@ -0,0 +1,15 @@
+//
+//  LTGradientProgressView.h
+//  GradientLayer&MaskLayer
+//
+//  Created by 梁天 on 16/10/9.
+//  Copyright © 2016年 梁天. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LTGradientProgressView : UIView
+
+@property (nonatomic, assign) CGFloat progress;
+
+@end
